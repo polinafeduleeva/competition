a5c1ed976a97c4731b613f6cf559dbc91b8b2d54
polinafeduleeva <131284959+polinafeduleeva@users.noreply.github.com>
2024-02-06 15:10:42 +0300

    Update predict_text.py

--- predict_text.py
+++ predict_text.py
@@ -6,6 +6,7 @@ import pytesseract
 model = YOLO('weights/best_50.pt')
 pytesseract.pytesseract.tesseract_cmd = r'C:\Tesseract\tesseract.exe'
 
+
 # Функция для определения текста на картинке
 def pred_text(img):
     # нужный объект находится на изображении с помощью нейросети и вырезается
@@ -13,16 +14,20 @@ def pred_text(img):
     if res:
         img = Image.open(img)
         box = [int(float(elem)) for elem in res[0].boxes.xyxy[0]]
-        img.crop(box)
+        img = img.crop(box)
         #  полученное изображение загружается в pytesseract, где определяется текст
-        txt = pytesseract.image_to_string(img, lang='eng', config='-c tessedit_char_whitelist=0123456789 --psm 11 --oem 3')
+        txt = pytesseract.image_to_string(img, lang='eng',
+                                          config='-c tessedit_char_whitelist=0123456789 --psm 11 --oem 3')
 
         txt = txt.replace(' ', '')
         txt = txt.replace('\n', '')
         txt = txt.replace('\t', '')
-        return txt
+        if txt:
+            return txt
+        else:
+            return 'текст не распознан'
 
 
 if __name__ == '__main__':
-    print(pred_text("test/images/20210806_17_07_31_000_wFlY39NOieR2fsNI4wU820Izeog2_F_3456_4608_jpg.rf.1ca3809a0ce2c80b79ca80a65ff10c20.jpg"))
-
+    print(pred_text("test/images/20210806_17_07_31_000_wFlY39NOieR2fsNI4wU820Izeog2_F_3456_4608_jpg.rf."
+                    "1ca3809a0ce2c80b79ca80a65ff10c20.jpg"))
