d07c70c90a0e241866c9439a284d72fdbec26b6b
polinafeduleeva <131284959+polinafeduleeva@users.noreply.github.com>
2024-02-06 15:11:10 +0300

    Update Qt_interface.py

--- Qt_interface.py
+++ Qt_interface.py
@@ -4,35 +4,60 @@ from predict_text import pred_text
 import sys
 
 
+WINDOW_W = 650
+WINDOW_H = 700
+
+
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(100, 100, 450, 500)
+        self.setGeometry(100, 100, WINDOW_W, WINDOW_H)
         self.setWindowTitle('тестирование нейронной сети')
         self.img_name = None
         self.label_img = QLabel(self)
-        self.label_img.move(10, 10)
+
+        self.status_label = QLabel(self)
+        self.status_label.resize(500, 30)
+        self.status_label.move(200, 10)
+        self.status_label.setText('Загрузите файл для распознавания')
+
+        self.res_label = QLabel(self)
+        self.res_label.resize(500, 30)
+        self.res_label.move(200, 490)
         # кнопка, вызывающая диалог для выбора картинки
         self.file_button = QPushButton(self)
-        self.file_button.resize(150, 50)
-        self.file_button.move(150, 440)
+        self.file_button.resize(250, 50)
+        self.file_button.move(200, 640)
         self.file_button.setText('Загрузить файл')
         self.file_button.clicked.connect(self.choice_file)
         # кнопка, по которой начинается распознавание
         self.file_button = QPushButton(self)
-        self.file_button.resize(150, 50)
-        self.file_button.move(150, 380)
+        self.file_button.resize(250, 50)
+        self.file_button.move(200, 580)
         self.file_button.setText('Распознать текст')
         self.file_button.clicked.connect(self.predict)
 
     def choice_file(self):
+        # открывается диалог с выбором картинки. Для удобства картинка отображается в окне приложения
         self.img_name = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
-        img = QPixmap(self.img_name)
-        self.label_img.setPixmap(img)
-        self.label_img.resize(img.width(), img.height())
+        try:
+            img = QPixmap(self.img_name)
+            w, h = img.width(), img.height()
+            img = img.scaled(int(0.1 * w), int(0.1 * h))
+            self.label_img.setPixmap(img)
+            self.label_img.resize(img.width(), img.height())
+            self.label_img.move((WINDOW_W - img.width()) // 2, 50)
+            self.status_label.setText('Изображение успешно загружено')
+            self.res_label.setText('')
+        except Exception:
+            self.status_label.setText('Выбранный файл некорректен')
+
     def predict(self):
+        # Если изображение выбрано - запускается распознавание, иначе - надпись "изображение не выбрано"
         if self.img_name:
-            print(pred_text(self.img_name))
+            self.res_label.setText(f'Результат - {pred_text(self.img_name)}')
+        else:
+            self.status_label.setText("Изображение не выбрано")
 
 
 if __name__ == '__main__':
